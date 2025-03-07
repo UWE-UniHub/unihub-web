683418f2701b30c5642311c261cd66d93a1e17a7
Denis Zhmurenko <hi@dyzoon.dev>
2025-03-07 00:11:20 +0000

    reset error value when rerender is done to uploadcontent (#30)

--- src/components/AvatarUploadContent/AvatarUploadContent.tsx
+++ src/components/AvatarUploadContent/AvatarUploadContent.tsx
@@ -1,4 +1,4 @@
-import {FC, useState} from "react";
+import {FC, useEffect, useState} from "react";
 import {LoadingOutlined, PlusOutlined} from "@ant-design/icons";
 import styles from './AvatarUploadContent.module.css';
 
@@ -9,6 +9,12 @@ type Props = {
 
 export const AvatarUploadContent: FC<Props> = ({ url, loading }) => {
     const [error, setError] = useState(false);
+
+    const src = `${url}?${new Date().getTime()}`;
+
+    useEffect(() => {
+        setError(false);
+    }, [src]);
 
     if(!url || error) {
         return (
@@ -21,7 +27,7 @@ export const AvatarUploadContent: FC<Props> = ({ url, loading }) => {
 
     return (
         <img
-            src={`${url}?${new Date().getTime()}`}
+            src={src}
             alt="avatar"
             className={styles.image}
             onError={() => setError(true)}

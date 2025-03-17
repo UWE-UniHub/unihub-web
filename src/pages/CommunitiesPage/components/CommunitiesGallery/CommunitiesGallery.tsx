1885e9bb0b159001dd8029f1b424e6806cdf3abd
akusvvz <116110183+baktygaliyevv@users.noreply.github.com>
2025-03-17 17:58:30 +0000

    bio fixed and working properly

--- src/pages/CommunitiesPage/components/CommunitiesGallery/CommunitiesGallery.tsx
+++ src/pages/CommunitiesPage/components/CommunitiesGallery/CommunitiesGallery.tsx
@@ -13,11 +13,11 @@ type Props = {
 export const CommunitiesGallery: FC<Props> = ({ communities }) => (
     <div className={styles.container}>
         {communities.map((community) => (
-            <Link to={`/community/${community.id}`}>
+            <Link to={`/community/${community.id}`} style={{ minWidth: 0 }}>
                 <Card key={community.id} hoverable>
                     <Flex align="center" gap={8}>
-                        <CommunityAvatar community={community} version={0} />
-                        <Flex vertical gap={8}>
+                        <CommunityAvatar community={community} version={0} className={styles.avatar} />
+                        <Flex vertical gap={8} style={{ flex: '1 0', minWidth: 0}}>
                             <Flex align="center" gap={8}>
                                 <Typography.Title level={5}>{community.name}</Typography.Title>
                                 <Flex align="center" gap={4}>
@@ -25,7 +25,11 @@ export const CommunitiesGallery: FC<Props> = ({ communities }) => (
                                     <Typography.Text>{community.subscribers}</Typography.Text>
                                 </Flex>
                             </Flex>
-                            <Typography.Text className={styles.bio}>{community.bio}</Typography.Text>
+                            <div style={{ width: "100%" }}>
+                                <Typography.Text ellipsis>
+                                    {community.bio}
+                                </Typography.Text>
+                            </div>
                         </Flex>
                     </Flex>
                 </Card>

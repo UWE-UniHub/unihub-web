17789df67b94232336b7165d4a9604537f33a737
Nuraly <116110183+baktygaliyevv@users.noreply.github.com>
2025-03-18 13:24:54 +0000

    Bug fixes (#49)
    * Update base.ts
    
    * long bio to 1 row fix
    
    closes #47
    
    * bio fixed and working properly
    
    ---------
    
    Co-authored-by: Denis Zhmurenko <hi@dyzoon.dev>

--- mock/communities.ts
+++ mock/communities.ts
@@ -9,19 +9,19 @@ export default [
             {
                 "id": "foobar",
                 "name": "Foo Bar Community",
-                "bio": "We are Foo Bar Community, welcome!",
+                "bio": "We are Foo Bar Community, welcome!We are Foo Bar Community, welcome!We are Foo Bar Community, welcome!We are Foo Bar Community, welcome!We are Foo Bar Community, welcome!We are Foo Bar Community, welcome!We are Foo Bar Community, welcome!",
                 "subscribers": 567
             },
             {
                 "id": "foobar1",
                 "name": "Foo Bar Community",
-                "bio": "We are Foo Bar Community, welcome!",
+                "bio": "We are Foo Bar Community, welcome irfkr korkforfkork okro ko ko !",
                 "subscribers": 567
             },
             {
                 "id": "foobar2",
                 "name": "Foo Bar Community",
-                "bio": "We are Foo Bar Community, welcome!",
+                "bio": "We are Foo Bar Community, welcomeo ok ok ok ok ok o ko!",
                 "subscribers": 567
             },
             {

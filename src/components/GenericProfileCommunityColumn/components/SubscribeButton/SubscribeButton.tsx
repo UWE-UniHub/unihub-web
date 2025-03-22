e237634a3ae987d58c597a02eab61b9f0a690df8
Denis Zhmurenko <hi@dyzoon.dev>
2025-03-22 23:55:14 +0000

    minor fixes (#55)

--- src/components/GenericProfileCommunityColumn/components/SubscribeButton/SubscribeButton.tsx
+++ src/components/GenericProfileCommunityColumn/components/SubscribeButton/SubscribeButton.tsx
@@ -4,6 +4,11 @@ import {profilesProfileIdFollowPost} from "../../../../api/profiles/profilesProf
 import {profilesProfileIdFollowDelete} from "../../../../api/profiles/profilesProfileIdFollowDelete.ts";
 import {communitiesCommunityIdFollowPost} from "../../../../api/communities/communitiesCommunityIdFollowPost.ts";
 import {communitiesCommunityIdFollowDelete} from "../../../../api/communities/communitiesCommunityIdFollowDelete.ts";
+import {useOwnProfile} from "../../../../stores/OwnProfileStore.ts";
+import {useAuthModal} from "../../../LayoutWrapper/useAuthModal.ts";
+import {useProfileSubscriptions} from "../../../../queries/useProfileSubscriptions.ts";
+import {useProfileFollowers} from "../../../../queries/useProfileFollowers.ts";
+import {useCommunityFollowers} from "../../../../queries/useCommunityFollowers.ts";
 
 type Props = {
     type: 'profile' | 'community';
@@ -13,14 +18,31 @@ type Props = {
 }
 
 export const SubscribeButton: FC<Props> = ({ type, id, subscribed, onUpdate }) => {
+    const { profile } = useOwnProfile();
+    const { openModal } = useAuthModal();
     const { message } = App.useApp();
     const [loading, setLoading] = useState(false);
 
+    const { refetch: refetchProfileSubs } = useProfileSubscriptions(id, type === 'profile');
+    const { refetch: refetchProfileFol } = useProfileFollowers(id, type === 'profile');
+    const { refetch: refetchCommunityFol } = useCommunityFollowers(id, type === 'community');
+
+
     const handleSubscribe = () => {
+        if(!profile) {
+            return openModal('login');
+        }
+
         setLoading(true);
         (type === 'profile' ? profilesProfileIdFollowPost : communitiesCommunityIdFollowPost)(id).then(() => {
             void message.success('Subscribed!');
             onUpdate();
+            if(type === 'profile') {
+                void refetchProfileSubs();
+                void refetchProfileFol();
+            } else {
+                void refetchCommunityFol();
+            }
         }).catch((e) => {
             console.error(e);
             void message.error(`Error (${JSON.stringify(e)})`);

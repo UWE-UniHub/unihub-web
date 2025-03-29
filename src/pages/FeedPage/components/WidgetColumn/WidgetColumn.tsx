dd136aa5ccf3ac5cbb6bcb7393628725b98b7748
Denis Zhmurenko <hi@dyzoon.dev>
2025-03-29 17:59:24 +0000

    minor fixes to right column

--- src/pages/FeedPage/components/WidgetColumn/WidgetColumn.tsx
+++ src/pages/FeedPage/components/WidgetColumn/WidgetColumn.tsx
@@ -3,9 +3,13 @@ import {useOwnProfile} from "../../../../stores/OwnProfileStore.ts";
 import {Button, Card, Flex, Typography} from "antd";
 import {ProfileAvatar} from "../../../../components/ProfileAvatar/ProfileAvatar.tsx";
 import styles from './WidgetColumn.module.css'
+import {useNavigate} from "react-router";
+import {useAuthModal} from "../../../../components/LayoutWrapper/useAuthModal.ts";
 
 export const WidgetColumn: FC = () => {
     const { profile } = useOwnProfile();
+    const navigate = useNavigate();
+    const { openModal } = useAuthModal();
 
     if(profile) {
         return (
@@ -13,9 +17,28 @@ export const WidgetColumn: FC = () => {
                 <Flex align="center" vertical gap={16}>
                     <ProfileAvatar profile={profile} version={0} size={64} />
                     <Typography.Title level={3}>{profile.first_name} {profile.last_name}</Typography.Title>
-                    <Button type="primary" size="large" block>Create a post</Button>
+                    <Button
+                        type="primary"
+                        size="large"
+                        block
+                        onClick={() => navigate(`/profile/${profile?.id}`)}
+                    >Create a post</Button>
                 </Flex>
             </Card>
         )
     }
+
+    return (
+        <Card className={styles.container}>
+            <Flex align="center" vertical gap={16}>
+                <Typography.Title level={3}>New to UniHub?</Typography.Title>
+                <Button
+                    type="primary"
+                    size="large"
+                    block
+                    onClick={() => openModal('signup')}
+                >Create an account now!</Button>
+            </Flex>
+        </Card>
+    )
 }

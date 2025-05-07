de5b4c5ac630ab65a558fb707d0970c899cd1909
Denis Zhmurenko <hi@dyzoon.dev>
2025-05-07 19:47:23 +0100

    microfix 2)

--- src/components/PostEditableDropdown/PostEditableDropdown.tsx
+++ src/components/PostEditableDropdown/PostEditableDropdown.tsx
@@ -1,4 +1,4 @@
-import {FC} from "react";
+import {FC, useState} from "react";
 import {DeleteOutlined, EditOutlined, EllipsisOutlined} from "@ant-design/icons";
 import {Button, Dropdown, MenuProps} from "antd";
 
@@ -8,6 +8,8 @@ type Props = {
 }
 
 export const PostEditableDropdown: FC<Props> = ({ onEdit, onDelete }) => {
+    const [open, setOpen] = useState(false);
+
     const items: MenuProps['items'] = [
         {
             key: 'edit',
@@ -31,8 +33,16 @@ export const PostEditableDropdown: FC<Props> = ({ onEdit, onDelete }) => {
     ]
 
     return (
-        <Dropdown trigger={['click']} menu={{ items }}>
-            <Button type="text" icon={<EllipsisOutlined rotate={90} style={{ fontSize: '16px' }} />} />
+        <Dropdown
+            menu={{ items }}
+            open={open}
+            onOpenChange={setOpen}
+        >
+            <Button
+                type="text"
+                icon={<EllipsisOutlined rotate={90} style={{ fontSize: '16px' }} />}
+                onClick={(e) => { e.stopPropagation(); setOpen(true); }}
+            />
         </Dropdown>
     )
 }

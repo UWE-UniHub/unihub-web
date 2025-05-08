0733b9319f604c1e1b2bad2f55a7f9cbff9ce607
Denis Zhmurenko <d.zhmurenko@dodobrands.io>
2025-05-08 13:27:25 +0100

    hotfix

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
   return {
     server: {
+      host: true,
       ...(env.VITE_PROXY ? {
         proxy: {
           ...(!env.VITE_PROXY_TARGET ? ({
